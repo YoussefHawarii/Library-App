454275610db4bd4f9acb27c3720889957953b8c4
YoussefHawarii <youssefelhawary566@gmail.com>
2026-05-11 02:20:02 +0300

    add rateLimit

--- src/app.controller.js
+++ src/app.controller.js
@@ -10,17 +10,17 @@ import { createHandler } from "graphql-http/lib/use/express";
 import { schema } from "../src/app.Graphql_Schema.js";
 import { rateLimit } from "express-rate-limit";
 
-/* const limiter = rateLimit({
+const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
   max: 5, // limit each IP to 5 requests per windowMs
   message: "Too many requests from this IP, please try again after 5 minutes",
-}); */
+});
 
 const bootstrap = async (app, express) => {
   await connectDB();
   app.use(cors());
   app.use(helmet());
-  // app.use(limiter);
+  app.use(limiter);
   app.use(express.json());
   app.use(
     "/graphql",
